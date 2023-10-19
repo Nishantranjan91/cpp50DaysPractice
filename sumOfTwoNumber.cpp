7b0bd9b11d18f011ccdc4238768a14f60a7ff75b
unknown <nishantranjj91555@gmail.com>
2023-10-19 16:48:31 +0530

    sumOfTwoNumber

---
+++ sumOfTwoNumber.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int sum;
+    int a;
+    cout << "Enter the first Number: ";
+    cin  >>a;
+    int b;
+    cout << "Enter the Second Number: ";
+    cin >> b;
+    sum = a + b;
+
+    cout<<"Your Sum is : "<<sum;
+}
