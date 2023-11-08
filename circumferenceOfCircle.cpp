def3417ae034aa264691b874c732057e13e10701
Nishant Ranjan <138917240+Nishantranjan91@users.noreply.github.com>
2023-11-08 18:43:32 +0530

    Add files via upload

---
+++ circumferenceOfCircle.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int radius = 10;
+    float pi = 3.1415;
+    float circumference = 2*3.1415*radius;
+    cout<<"Your circumference is: "<<circumference;
+}
---
+++ differencebetweenasciiof2num.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main (){
+    char ch1;
+    cout<<"Enter first character: ";
+    cin>>ch1;
+    char ch2;
+    cout<<"Enter second character: ";
+    cin>>ch2;
+    int x = (int)ch1;
+    int y = (int)ch2;
+    cout<< x-y ;
+}
