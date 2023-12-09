0082dd46960650948b6d5f42084b3a0bf2928852
unknown <nishantranjj91555@gmail.com>
2023-12-09 23:05:34 +0530

    typecasting

---
+++ Day_9/typecasting3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float x=7.1;
+    int y;
+    y=(int)x;
+    cout<<y;
+
+    int a = 7;
+    float b = 2.1;
+    int call = a + b;
+    cout<<call<<endl;
+    }
