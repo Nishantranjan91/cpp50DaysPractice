325eefcda7cf3778f850e1e2d86074c176483b3e
unknown <nishantranjj91555@gmail.com>
2024-01-10 22:36:48 +0530

    evenoddusingnesteeif

---
+++ Day_23/evenOrOdd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter your number:";
+    cin>>n;
+    if(n%2==0)
+    cout<<"the given number is even";
+    if(n%2!=0)
+    cout<<"the given number is odd";
+
+
+}
---
+++ Day_23/volumeOfTheCylinder.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<math.h>
+using namespace std; 
+int main(){
+    int radius;
+    int height;
+    float pi= 3.14;
+    float volume=pi*radius*radius*height;
+    cout<<"enter the value of radius:";
+    cin>>radius;
+    cout<<"enter the value  of height:";
+    cin>>height;
+    cout<<volume;
+}  
