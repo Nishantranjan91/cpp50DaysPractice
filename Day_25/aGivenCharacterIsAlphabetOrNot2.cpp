1a125d921e16311ffba504b541fb530bf84a67bd
unknown <nishantranjj91555@gmail.com>
2024-01-12 23:29:54 +0530

    nested if else

---
+++ Day_25/aGivenCharacterIsAlphabetOrNot2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"enter the character:";
+    cin>>ch;
+    int ascii=(int)ch;
+    if(ascii>=97 && ascii<=122 || ascii>=65 && ascii<=90){
+        cout<<"the given character is a  alphabet";
+    }
+    else{
+        cout<<"the given character is not an alphabet";
+    }
+    
+}
