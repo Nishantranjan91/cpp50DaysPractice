b87a1a827ba0ae5de2fcae09314091225bfdbd6a
unknown <nishantranjj91555@gmail.com>
2024-01-13 23:03:45 +0530

    if else

---
+++ Day_26/givenCharacterIsVowelOrConsonent.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout << "enter your character:";
+    cin >> ch;
+    int ascii = (int)ch;
+    if ((ascii >= 97 && ascii <= 122) || (ascii >= 65 && ascii <= 90))
+    {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+        {
+            cout << "the given character is a vowel";
+        }
+        else
+        {
+            cout << "the given character is a consonant";
+        }
+    }
+    else
+    {
+        cout << "the given character is not an alphabet";
+    }
+}
