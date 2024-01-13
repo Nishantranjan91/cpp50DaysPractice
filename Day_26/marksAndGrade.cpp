7a994e40b9394912a57fd4889cfccaf55cebd976
unknown <nishantranjj91555@gmail.com>
2024-01-13 20:24:04 +0530

    else if

---
+++ Day_26/marksAndGrade.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter your marks:";
+    cin>>n;
+    if(n>=91 && n<=100){
+        cout<<"Excellent";
+    }
+    if(n>=81 && n<=90){
+        cout<<"very good";
+    }
+    if(n>=71 && n<=80){
+        cout<<"good";
+    }
+    if(n>=61 && n<=70){
+        cout<<"can do better";
+    }
+    if(n>=51 && n<=60){
+        cout<<"average";
+    }
+    if(n>=40 && n<=50){
+        cout<<"below average";
+    }
+    if(n<40){
+        cout<<"fail";
+    }
+}
