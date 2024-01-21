ce81bd380a2f1035dee64af36c9f31bed83bf32b
unknown <nishantranjj91555@gmail.com>
2024-01-21 20:25:28 +0530

    basic

---
+++ Day_34/basicOutPut5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=65;
+    char j='A';
+    if(i==j)
+    cout<<"p stands for gec jamui";
+    else{
+        cout<<"p stand for parameter";
+        
+    }
+}
