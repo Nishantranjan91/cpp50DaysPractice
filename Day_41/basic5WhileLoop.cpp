ab3845443ccf5eb6c3f236f37d1b7769afea8426
unknown <nishantranjj91555@gmail.com>
2024-01-28 11:27:52 +0530

    while Loop

---
+++ Day_41/basic5WhileLoop.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    while(x=0;x<=255;x++){
+        cout<<"ascii value of character "<<(char)('a+x');
+        cout<<"is"<<x<<endl;
+    }
+    
+} 
