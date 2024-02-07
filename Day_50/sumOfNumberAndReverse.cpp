051283a5874a3653dd7e843c4f842b232d9b5822
unknown <nishantranjj91555@gmail.com>
2024-02-07 19:58:33 +0530

    while loop

---
+++ Day_50/sumOfNumberAndReverse.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a number:";
+    cin>>n;
+    int r=0;
+    int x=n;
+    while(n>0){
+        int id=n%10;
+        r*=10;
+        r+=id;
+        n/=10;
+    }
+    cout<<r+x;
+    }
