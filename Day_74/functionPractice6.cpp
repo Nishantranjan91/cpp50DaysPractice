9d2ea8f3ad93058a635c095e499949461fe226cf
unknown <nishantranjj91555@gmail.com>
2024-03-02 17:13:12 +0530

    function

---
+++ Day_74/functionPractice6.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+void greeting(){
+    cout<<"good morning ";
+    cout<<"have a nice day "<<endl;
+}
+int main(){
+    greeting();
+    greeting();
+    greeting();
+}
