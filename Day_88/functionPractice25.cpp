3414dd6e8596fb8446cb6e31bf2f1c5a96cf5673
unknown <nishantranjj91555@gmail.com>
2024-03-16 19:05:25 +0530

    function

---
+++ Day_88/functionPractice25.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int maxthree(int a, int b, int c){
+    int max;
+    if(a>b && a>c){
+        max=a;
+    }
+    else if (b>c && b>a){
+        max=b;
+    }
+    else max = c;
+    return max;
+}
+int main(){
+    int a=2, b=9, c=7;
+    cout<<maxthree(a,b,c);
+}
