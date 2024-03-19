28bfe4ff5726bc3f24aaba4aa29a6b752741eada
unknown <nishantranjj91555@gmail.com>
2024-03-19 23:53:10 +0530

    test

---
+++ Day_91/randomTest15.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int gcd(int a, int b){
+    int hcf =1;
+    for(int i=1; i<=min(a,b); i++){
+        if(a%i==0&&b%i==0){
+            hcf=i;
+        }
+    }
+    return hcf;
+}
+int main(){
+    int a;
+    cout<<"enter the first number:";
+    cin>>a;
+    int b;
+    cout<<"enter the second number:";
+    cin>>b;
+    cout<<gcd(a,b);
+}
