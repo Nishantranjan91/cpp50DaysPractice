7d8feaf046b649c00b180c8e5185b3b0e01f8365
unknown <nishantranjj91555@gmail.com>
2024-03-21 09:02:08 +0530

    function

---
+++ Day_93/AllOddNumbersBetweenAandB.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter the first number:";
+    cin>>a;
+    int b;
+    cout<<"enter the second number:";
+    cin>>b;
+    for(int i=a; i<=b; i++){
+        cout<<i<<" ";
+    }
+}
