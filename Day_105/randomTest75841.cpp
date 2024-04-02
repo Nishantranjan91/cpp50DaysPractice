f86fbaefbdfcc15766a054802c1738378fc3fec3
unknown <nishantranjj91555@gmail.com>
2024-04-02 19:21:23 +0530

    test

---
+++ Day_105/randomTest75841.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int radius;
+    cout<<"enter the radius:";
+    cin>>radius;
+    float pi=3.14;
+    float circumference,area;
+    area=pi*radius*radius;
+    circumference=2*pi*radius;
+    if(circumference>area){
+        cout<<"greater is circumference"<<circumference;
+    }
+    if(area>circumference){
+        cout<<"greatest is area"<<area;
+    }
+    if(area==circumference){
+        cout<<"both are equal";
+    }
