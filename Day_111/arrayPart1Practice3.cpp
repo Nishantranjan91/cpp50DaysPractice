cbf329168fe276aa5267335251a4a932aaff3b96
unknown <nishantranjj91555@gmail.com>
2024-04-08 10:06:37 +0530

    arrays

---
+++ Day_111/arrayPart1Practice3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[7];
+    arr[0]=7;
+    arr[1]=1;
+    arr[2]=6;
+    arr[3]=3;
+    arr[4]=9;
+    arr[5]=7;
+    arr[6]=0;
+    cout<<arr[0];
+}
