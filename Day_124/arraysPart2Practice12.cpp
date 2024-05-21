45b7773250cb0eded0545ae72b6444d4ed54e127
unknown <nishantranjj91555@gmail.com>
2024-05-21 20:40:48 +0530

    test

---
+++ Day_124/arraysPart2Practice12.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    int x,y;
+    x=6,y=6;
+    cout<<x<<endl;
+    cout<<x++<<endl;
+    cout<<x--<<endl;
+    cout<<--x<<endl;
+    cout<<++x;
+}
--- Day_81/functionPractice22.cpp
+++ Day_81/functionPractice22.cpp
@@ -1,7 +1,7 @@
-#include<iostream>
+#   int x=3;
+   include<iostream>
 using namespace std;
 int main(){
-    int x=3;
-    int y=7;
+  int y=7;
     cout<<&x;
 }
