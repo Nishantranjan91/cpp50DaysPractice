2e7879e4985ec440ec7c935904e44140dac97452
unknown <nishantranjj91555@gmail.com>
2024-09-17 19:21:39 +0530

    basic

---
+++ Day_125/basic_printing.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"my college best friend is santosh kumar";
+    cout<<endl;
+    cout<< "santosh is a very strict student";
+    cout<<"\n";
+    cout<<"santosh and nishant both are room partner";
+    cout<<"\nsantosh kumar is belongs to muzaffarpur district\n";
+    cout<<"nishant ranjan is belongs to sheikhpura district";
+}
