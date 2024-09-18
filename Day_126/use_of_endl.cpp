026bdec7caf9c35492ba8eb88f41f7c72606ef77
unknown <nishantranjj91555@gmail.com>
2024-09-18 10:13:59 +0530

    endl

--- Day_126/use_of_endl.cpp
+++ Day_126/use_of_endl.cpp
@@ -1,9 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"gecJamui";
-    cout<<endl;
-    cout<<endl;
-    cout<<endl;
+    cout<<"gecJamui"<<endl;
     cout<<"beu university";
 }
