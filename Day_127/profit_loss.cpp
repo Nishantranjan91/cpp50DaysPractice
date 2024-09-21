596080e23c3936eb10f1c0e90ad5dada082a3d63
unknown <nishantranjj91555@gmail.com>
2024-09-21 08:14:07 +0530

    character or not

--- Day_127/profit_loss.cpp
+++ Day_127/profit_loss.cpp
@@ -7,11 +7,17 @@ int main(){
     cout<<"enter the selling price:";
     int sp;
     cin>>sp;
+    int profit=sp-cp;
+    int loss=cp-sp;
     if(sp>cp){
         cout<<"you are in profit";
+        cout<<endl;
+        cout<<"your profit is:"<<profit;
     }
     else{
         cout<<"you are in loss";
+        cout<<endl;
+        cout<<loss;
     }
     
 }
