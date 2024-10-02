7c70b120e6fff15626fc2592eb78e393804e391c
unknown <nishantranjj91555@gmail.com>
2024-10-02 18:15:33 +0530

    previous

--- Day_133/odd_number.cpp
+++ Day_133/odd_number.cpp
@@ -1,10 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-    for(int i=1;i<=10;i++){
-        if(i%2!=0){
-            cout<<i<<endl;
-        }
-            
+    for(int i=1;i<=10;i+=2){
+        cout<<i<<endl;
     }
+            
+    
 }
